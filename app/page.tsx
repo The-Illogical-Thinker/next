9fbb3cd18e15f1b284e3a3776a77060809c094f0
The-Illogical-Thinker <tanmoybortamuli@gmail.com>
2026-01-29 07:30:55 +0530

    added session logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
-export default function Home() {
-    return <div>
+import { redirect } from "next/navigation"
+import { headers } from "next/headers"
+import { auth } from "@/lib/auth";
+import { useRouter } from "next/router";
+import { SignoutButton } from "@/components/buttons";
 
+export default async function Home() {
+
+    const header = await headers();
+
+    const router = useRouter();
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    if (!session) {
+        console.log(session)
+        return redirect("/signin")
+    }
+
+    return <div>
+        Hello there {session.user.name}
+        <br />
+        <SignoutButton>Click here to signout</SignoutButton>
     </div>
 }
